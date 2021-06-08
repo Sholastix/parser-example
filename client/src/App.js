ba86fc60c0738868c01f22d020ac5b24f0b95ed1
Sholastix <drenai6436@gmail.com>
2021-06-08 12:22:38 +0300

    'client' skeleton added

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import { Button, Form, FormGroup, Input, Label, Modal, ModalHeader, ModalBody, ModalFooter, Table } from 'reactstrap';
+
+import './App.css';
+
+const App = () => {
+
+    return (
+        <div className='App container'>
+
+        </div>
+    );
+}
+
+export default App;
